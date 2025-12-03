50cb90d10b0fd9ad527c40021812ad1ea6ac3487
Nikdane12 <122749614+Nikdane12@users.noreply.github.com>
2025-12-03 01:11:26 +0100

    Update scores.js

--- scores.js
+++ scores.js
@@ -1,11 +1,13 @@
 const scores = [
-    { name: "Sonja", points: 3 },
-    { name: "Anette", points: 3 },
+    { name: "Sonja", points: 6 },
+    { name: "Anette", points: 6 },
     { name: "Lars", points: 3 },
-    { name: "Marte", points: 3 },
-    { name: "Linn", points: 3 },
+    { name: "Marte", points: 6 },
+    { name: "Linn", points: 4 },
     { name: "Margot", points: 0 },
-    { name: "Silvia", points: 3 },
+    { name: "Silvia", points: 6 },
     { name: "Finn", points: 3 },
+    { name: "Matt", points: 3 },
+    { name: "Estelle", points:3 }
     
 ]
