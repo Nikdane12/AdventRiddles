4bd09489bd152ef91c9b44b806288e88b0a26621
Nikolai <nikolai.dickens00@gmail.com>
2025-12-06 02:12:31 +0100

    06ScoreUpdate

--- scores.js
+++ scores.js
@@ -1,13 +1,13 @@
 const scores = [
-    { name: "Anette", points: 9 },
-    { name: "Sonja", points: 9 },
+    { name: "Anette", points: 12 },
+    { name: "Sonja", points: 12 },
     { name: "Lars", points: 8 },
     { name: "Marte", points: 9 },
-    { name: "Linn", points: 7 },
+    { name: "Linn", points: 10 },
     { name: "Margot", points: 4 },
     { name: "Silvia", points: 9 },
     { name: "Finn", points: 9 },
     { name: "Matt", points: 6 },
-    { name: "Estelle", points:8 }
-    
+    { name: "Estelle", points: 8 },
+    { name: "Isolde", points: 6 },
 ]
